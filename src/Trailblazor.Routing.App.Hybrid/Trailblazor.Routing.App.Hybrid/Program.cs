12b8b2c24bc10b0d2a34413eea2d93640cf9b39e
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-06 17:43:10 +0200

    Added all Blazor variants as test apps #41

---
+++ src/Trailblazor.Routing.App.Hybrid/Trailblazor.Routing.App.Hybrid/Program.cs
@@ -0,0 +1,35 @@
+namespace Trailblazor.Routing.App.Hybrid;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+        builder.Services.AddApp();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+        app.MapRazorComponents<LandingPage>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode();
+
+        app.Run();
+    }
+}
