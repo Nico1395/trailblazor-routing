2d365ae942f449c5c01e71ccf013eb9846622ab6
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-11 15:03:32 +0200

    Added route attributes and sample components #11

---
+++ src/Trailblazor.Routing.App/Samples/AttributeSamples/AttributeSampleChild.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Trailblazor.Routing.App.Samples.AttributeSamples;
+
+[Route("attribute-sample-child")]
+[RouteParent(typeof(AttributeSample))]
+public partial class AttributeSampleChild : ComponentBase
+{
+    [Inject]
+    private INavigator Navigator { get; set; } = null!;
+
+    [CascadingParameter]
+    public required RouterContext RouterContext { get; set; }
+
+    private string? GetTitle()
+    {
+        return RouterContext.Route?.GetMetadataValue<string?>("title");
+    }
+
+    private void NavigateToParent()
+    {
+        Navigator.NavigateTo<AttributeSample>();
+    }
+}
